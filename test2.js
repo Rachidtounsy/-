f12b849b318f140115965499882df4ed1d784f8f
Rachid Tounsy <74862454+Rachidtounsy@users.noreply.github.com>
2021-01-25 15:36:35 +0100

    Add files via upload
    محرر اكواد يغنيك عن  تثبيث اي محرر اخر على حاسوبك

---
+++ test2.js
@@ -0,0 +1,13 @@
+
+function   PlayCodes()
+{
+ var Codes=  document.getElementById("Codes-textarea").value;
+document.getElementById("Output_id").innerHTML= Codes; 
+
+
+}
+
+function   RemoveCodes()
+{
+    document.getElementById("Output_id").innerHTML="";
+}
